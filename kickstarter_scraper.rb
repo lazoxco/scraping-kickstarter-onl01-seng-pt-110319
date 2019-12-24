700a456aa9d1d06ff7e7943f90077d026256e9eb
Nory Lazo <nlazo@icloud.com>
2019-12-24 04:44:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,8 +7,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
+  
+  binding.pry 
+
 end
 
-binding.pry 
-
-create_project_hash.new
+create_project_hash
